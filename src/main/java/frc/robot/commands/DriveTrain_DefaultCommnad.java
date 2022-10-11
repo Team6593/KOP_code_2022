55560c06123e1dd13cc2860500ab72d20d75f022
KelvinBM <kelvinmejia182@gmail.com>
2022-10-11 16:52:43 -0400

    added DriveTrain command, still needs somethings added

--- src/main/java/frc/robot/commands/DriveTrain_DefaultCommnad.java
+++ src/main/java/frc/robot/commands/DriveTrain_DefaultCommnad.java
@@ -4,23 +4,12 @@
 
 package frc.robot.commands;
 
-import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
-/** An example command that uses an example subsystem. */
-public class ExampleCommand extends CommandBase {
-  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
-  private final ExampleSubsystem m_subsystem;
-
-  /**
-   * Creates a new ExampleCommand.
-   *
-   * @param subsystem The subsystem used by this command.
-   */
-  public ExampleCommand(ExampleSubsystem subsystem) {
-    m_subsystem = subsystem;
+public class DriveTrain_DefaultCommnad extends CommandBase {
+  /** Creates a new DriveTrain_DefaultCommnad. */
+  public DriveTrain_DefaultCommnad() {
     // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(subsystem);
   }
 
   // Called when the command is initially scheduled.
